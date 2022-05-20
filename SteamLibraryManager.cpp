5642d59398208ab9224346d869f02449eeb360a1
Simi <simi.98@hotmail.com>
2022-05-21 00:13:40 +1000

    Updated readme with todolist and created testprogram

---
+++ SteamLibraryManager.cpp
@@ -0,0 +1,30 @@
+/*
+* Steam File Manager
+* Author: Simeon Pento
+* Last Modified: 20/05/2022
+*/
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+int main() {
+    string version = "ALPHA - v0.1";
+    cout << "Steam Library Manager - " << version << endl;
+
+    fstream file;
+    file.open("./appmanifests/appmanifest_60.acf", ios::in);
+
+    if(file.is_open()){
+        string line;
+         while ( getline (file, line) ){
+            cout << line << endl;
+        }
+    } else {
+        cout << "FILE FUCKY WUCKY NOT OPEN" << endl;
+        return -1;
+    }
+
+    file.close();
+    return 0;
+}
